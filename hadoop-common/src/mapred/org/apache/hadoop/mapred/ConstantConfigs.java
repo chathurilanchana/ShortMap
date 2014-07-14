1f54a7cf9b4c949dbe71552ddf4f637222659f8c
Chathuri <chathuri@ubuntu.(none)>
2014-07-15 02:07:50 +0530

    latest shortmap

---
+++ hadoop-common/src/mapred/org/apache/hadoop/mapred/ConstantConfigs.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.apache.hadoop.mapred;
+
+/**
+ * @author chathuri
+ *
+ */
+public class ConstantConfigs {
+	public static String FIRST_COLUMN_IDENTIFIER_TEXT="col0";
+	public static String NUM_INPUT_FILES = "mapreduce.input.num.files";
+	public static String SPECIFIC_PREFIX="ZblockZ";
+	public static String COLUMN_PREFIX="col";
+	public static String RELEVANT_ATTR_CONFIG="relevantAttrs";
+	public static String FILTER_VALUES_CONFIG="filterValues";
+	public static String OUTPUT_LOC_CONFIG="outputloc";
+}
